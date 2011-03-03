43ed95576aed4056a7bdc1fca82c81dd72329030
Mike Sarahan <msarahan@gmail.com>
2011-03-03 17:24:51 +0000

    First commit - version 1.0 of fast beam shutter control.  Includes multithreading and support for two input sync signals, as well as two output signals.

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+XC_SRCS += \
+../src/fbsMT.xc 
+
+OBJS += \
+./src/fbsMT.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.xc
+	@echo 'Building file: $<'
+	@echo 'Invoking: XC Compiler'
+	xcc -O0 -g -Wall -c -o "$@" "$<" "../XC-1A.xn"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/subdir.mk
@@ -0,0 +1,11 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+XN_SRCS += \
+../XC-1A.xn 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
